86dba3708c20e37f0f13925ce74df7b2c7ea1f8c
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2021-12-01 01:52:50 -0300

    Agregamos un banner slider de tiendamia

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -5,23 +5,33 @@ import {  Carousel} from 'react-bootstrap'
 function Banner() {
     return (          
         
-        <Carousel>
-        <Carousel.Item>
+        <Carousel style={{marginBottom:"30px"}}>
+            <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://images.fravega.com/f300/004f1aaa0069f554d93a8dda37e38f53.jpg.webp"
-            alt="First slide"
-          />
-          
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.fravega.com/f300/e3ccceb858bdce612ba6602053a62d38.png.webp"
+            src="https://images.tiendamia.com/d6432f78e5db3d20ebf61547a51e5cdd.png"
             alt="Second slide"
           />     
           
         </Carousel.Item>
+             <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.tiendamia.com/48b44c89441ed8e23b668ecf48f9502a.png"
+            alt="Second slide"
+          />     
+          
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.tiendamia.com/57f268e88946a83f48e6e37cefbc55fe.png"
+            alt="First slide"
+          />
+          
+        </Carousel.Item>
+        
+       
        
       </Carousel>
     )
