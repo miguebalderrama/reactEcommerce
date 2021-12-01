ada6af116dfa9030c0544988c5ef92eb3fae7960
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2021-12-01 01:45:02 -0300

    Agregamos un banner slider

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import {  Image} from 'react-bootstrap'
+import {  Carousel} from 'react-bootstrap'
 
 
 function Banner() {
-    return (
-        <div>
-        <Image src="https://images.fravega.com/f300/004f1aaa0069f554d93a8dda37e38f53.jpg.webp" fluid expand="lg"/>
-   
-        </div>
+    return (          
+        
+        <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.fravega.com/f300/004f1aaa0069f554d93a8dda37e38f53.jpg.webp"
+            alt="First slide"
+          />
+          
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.fravega.com/f300/e3ccceb858bdce612ba6602053a62d38.png.webp"
+            alt="Second slide"
+          />     
+          
+        </Carousel.Item>
+       
+      </Carousel>
     )
 }
 
